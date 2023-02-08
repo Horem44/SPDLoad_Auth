75d6aa8be7597020b7b2a6f390b59ee81344b511
Ilya <Yegorov>
2023-02-08 12:00:07 +0200

    signup

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/users/users.entity';
 import { Repository } from 'typeorm';
@@ -12,16 +12,38 @@ export class AuthService {
     private usersRepository: Repository<Users>,
   ) {}
 
-  async signup(dto: SignupDto): Promise<Users>{
+  async signup(dto: SignupDto) {
     const hash = await argon.hash(dto.password);
 
-    const user = await this.usersRepository.save({
-      lastName: dto.lastName,
-      firstName: dto.firstName,
-      email: dto.email,
-      hash,
-    });
+    try {
+      const existingUser = await this.usersRepository.findOne({
+        where: {
+          email: dto.email,
+        },
+      });
 
-    return user;
+      if (existingUser) {
+        throw new ForbiddenException('Credentials taken');
+      }
+
+      const user = await this.usersRepository.save({
+        lastName: dto.lastName,
+        firstName: dto.firstName,
+        email: dto.email,
+        hash,
+      });
+
+      return user;
+    } catch (err) {
+      if (!err.response) {
+        return {
+          statusCode: 500,
+          message: 'Internal server error',
+          error: 'Server Error',
+        };
+      }
+
+      return err.response;
+    }
   }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/users/users.entity';
 import { Repository } from 'typeorm';
-import { UserDto } from './dto';
 
 @Injectable()
 export class UserService {
